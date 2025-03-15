62ea1c7e924423441caf8ae353014fae2305922b
Daniel Azoulay <danielazoulay2010@gmail.com>
2025-03-15 09:56:08 -0300

    feat(layout): add RootLayout component with global styles and font configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Montserrat, Oxanium } from 'next/font/google'
+import './global.css'
+
+export const metadate: Metadata = {
+	title: 'Devstage',
+}
+
+const oxanium = Oxanium({
+	weight: ['500', '600'],
+	subsets: ['latin'],
+	variable: '--font-montserrat',
+})
+
+const montserrat = Montserrat({
+	weight: ['400', '600'],
+	subsets: ['latin'],
+	variable: '--font-montserrat',
+})
+
+export default function RootLayout({
+	children,
+}: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang="pt-BR" className={`${oxanium.variable} ${montserrat.variable}`}>
+			<body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top">
+				<main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+					{children}
+				</main>
+			</body>
+		</html>
+	)
+}
